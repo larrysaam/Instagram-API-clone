2301e64ad32ce63ee911c3fc01d69245b4c56647
larrysaam <nyaah.saam@ictuniversity.edu.cm>
2024-02-06 21:22:58 +0100

    done

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next)=>{
     try {
         
         const token = req.headers.authorization.split(" ")[1]
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
+        const decoded = jwt.verify(token, "saamslarrien")
         req.userData = decoded
         next()
     } catch (error) {
