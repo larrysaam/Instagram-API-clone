cc43dce7f2fcdea2090b5a8127a1cae41f7a0410
larrysaam <nyaah.saam@ictuniversity.edu.cm>
2024-11-25 15:47:39 +0100

    new

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next)=>{
-    try {
-        
+    try { 
         const token = req.headers.authorization.split(" ")[1]
         const decoded = jwt.verify(token, "saamslarrien")
         req.userData = decoded
